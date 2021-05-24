74045e32954b76ad33808bb51a496527e7c5f581
Your Name <you@example.com>
2021-05-25 01:07:50 +0200

    this is commit kkkddhh

--- admin/includes/init2.php
+++ admin/includes/init2.php
@@ -1,1 +1,18 @@
-<?php require_once("config.php"); ?>
+<?php 
+
+require_once("functions.php");
+require_once("config.php");
+require_once("database.php");
+require_once("db_object.php");
+require_once("user.php");
+require_once("photo.php");
+require_once("comment.php");
+require_once("session.php");
+require_once("paginate.php");
+
+
+
+
+
+
+?>
