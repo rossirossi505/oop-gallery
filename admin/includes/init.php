a0f37412424423983dcad07b79372f5e52c40d43
Your Name <you@example.com>
2021-05-25 00:37:33 +0200

    this is commitdh kk

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -2,15 +2,15 @@
 
 
 
-defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+//defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-define('SITE_ROOT', 'C:' . DS . 'XAMPP' . DS . 'htdocs' . DS . 'gallery' );
+//define('SITE_ROOT', 'C:' . DS . 'XAMPP' . DS . 'htdocs' . DS . 'gallery' );
 
 
-defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT.DS.'admin'.DS.'includes');
+//defined('INCLUDES_PATH') ? null : define('INCLUDES_PATH', SITE_ROOT.DS.'admin'.DS.'includes');
 
-defined('IMAGES_PATH') ? null : define('IMAGES_PATH', SITE_ROOT.DS.'admin'.DS.'images');
-
+//defined('IMAGES_PATH') ? null : define('IMAGES_PATH', SITE_ROOT.DS.'admin'.DS.'images');
+/*
 require_once(INCLUDES_PATH.DS."functions.php");
 require_once(INCLUDES_PATH.DS."config.php");
 require_once(INCLUDES_PATH.DS."database.php");
@@ -20,6 +20,16 @@ require_once(INCLUDES_PATH.DS."photo.php");
 require_once(INCLUDES_PATH.DS."comment.php");
 require_once(INCLUDES_PATH.DS."session.php");
 require_once(INCLUDES_PATH.DS."paginate.php");
+*/
+require_once("functions.php");
+require_once("config.php");
+require_once("database.php");
+require_once("db_object.php");
+require_once("user.php");
+require_once("photo.php");
+require_once("comment.php");
+require_once("session.php");
+require_once("paginate.php");
 
 
  ?>
